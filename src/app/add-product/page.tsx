a3a24fd84d252fe9679f976b31f37c6767d2e3bf
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-08-09 23:55:19 +0700

    Add Product & FormSubmitButton components

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,11 +1,36 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prisma"
+import { redirect } from "next/navigation"
+
 export const metadata = {
   title: 'Add Product - Gadget Zone',
 }
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+  await prisma.product.create({
+    data: {
+      name, description, imageUrl, price
+    }
+  })
+
+  redirect("/")
+}
+
 export default function AddProductPage() {
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold">Add-product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           className="mb-3 w-full input input-bordered"
           required
@@ -28,7 +53,11 @@ export default function AddProductPage() {
           name='price'
           type="number"
           placeholder="Price" />
-        <button className="btn btn-primary btn-block" type="submit">Add Product</button>
+        <FormSubmitButton
+          className="btn-block"
+          type="submit">
+          Add Product
+        </FormSubmitButton>
       </form>
     </div>
   )
