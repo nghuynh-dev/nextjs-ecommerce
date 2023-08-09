7ed38a96aaa0ace55af1ba80a512702cfe86b056
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-08-09 23:54:15 +0700

    create error skeleton, config and fix warning on consolog

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [{ hostname: "images.unsplash.com" }]
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
