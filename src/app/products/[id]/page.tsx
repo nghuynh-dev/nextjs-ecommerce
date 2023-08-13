99f144b9835b7f7c55fca26c3e26f3df76459d9d
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-08-13 11:48:07 +0700

    Metadata product details page + React.cache

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/db/prisma"
 import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation"
+import { cache } from "react";
 
 interface ProductPageProps {
   params: {
@@ -10,11 +11,33 @@ interface ProductPageProps {
   }
 }
 
-export default async function ProductPage({ params: { id } }: ProductPageProps) {
-  const product = await prisma.product.findUnique({
-    where: { id }
-  })
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } })
   if (!product) notFound();
+  return product;
+})
+
+export async function generateMetadata(
+  { params: { id } }: ProductPageProps
+): Promise<Metadata> {
+  const product = await getProduct(id)
+
+
+  return {
+    title: product.name + " - GadgetZone",
+    description: product.description,
+    openGraph: {
+      images: [{
+        url: product.imageUrl
+      }]
+    }
+  }
+}
+
+export default async function ProductPage(
+  { params: { id } }: ProductPageProps
+) {
+  const product = await getProduct(id)
 
   return (
     <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
