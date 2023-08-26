1ab1c56120056a5bb9e96313a40069d812007856
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-08-26 11:09:39 +0700

    Navbar + Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from "@/app/Navbar/navbar";
+import Footer from "@/app/Footer";
+import React from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={inter.className}>
+      <Navbar />
         <main className="m-auto min-w-[300px] max-w-7xl p-4">
           {children}
         </main>
+      <Footer />
       </body>
     </html>
   )
